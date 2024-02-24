9394dac565a5faa37e5c1bbbf140e6158e354bba
Scott Beru <scottdreu@gmail.com>
2024-02-24 22:15:36 +0100

    Add Player class and delegate data input to it

--- web/main.py
+++ web/main.py
@@ -2,11 +2,21 @@ from weapon_optimizer_v2 import *
 from data_puller import *
 from AttackElementCorrectParam import AttackElementCorrectParam
 from CalcCorrectGraph import CalcCorrectGraph
+from dataclasses import dataclass
 from nicegui import ui, run
 
 player_stats = {}
 PLAYER_STATS = ['str', 'dex', 'int', 'fai', 'arc']
 DMG_TYPES = ['phys', 'magic', 'fire', 'lightning', 'holy']
+
+@dataclass
+class Player:
+    strength: int = 0
+    dexterity: int = 0
+    intelligence: int = 0
+    faith: int = 0
+    arcane: int = 0
+    max_weight: float = 0
 
 @ui.page('/')
 def index():
@@ -18,27 +28,32 @@ def index():
         result = await run.cpu_bound(optimizer, player_stats)
         ui.label(result)
 
-    strength = ui.number(label='Strength (STR)', value=10, min=1, max=99, precision=0).props('clearable')
+    ui.number(label='Strength (STR)', value=10, min=1, max=99, precision=0).props('clearable').bind_value(player, 'strength')
 
-    dexterity = ui.number(label='Dexterity (DEX)', value=10, min=1, max=99, precision=0).props('clearable')
-    player_stats['dex'] = dexterity
+    ui.number(label='Dexterity (DEX)', value=10, min=1, max=99, precision=0).props('clearable').bind_value(player, 'dexterity')
 
-    intelligence = ui.number(label='Intelligence (INT)', value=10, min=1, max=99, precision=0).props('clearable')
-    player_stats['int'] = intelligence
+    ui.number(label='Intelligence (INT)', value=10, min=1, max=99, precision=0).props('clearable').bind_value(player, 'intelligence')
 
-    faith = ui.number(label='Faith (FAI)', value=10, min=1, max=99, precision=0).props('clearable')
-    player_stats['fai'] = faith
+    ui.number(label='Faith (FAI)', value=10, min=1, max=99, precision=0).props('clearable').bind_value(player, 'faith')
 
-    arcane = ui.number(label='Arcane (ARC)', value=10, min=1, max=99, precision=0).props('clearable')
-    player_stats['arc'] = arcane
+    ui.number(label='Arcane (ARC)', value=10, min=1, max=99, precision=0).props('clearable').bind_value(player, 'arcane')
 
-    weight = ui.number(label='Maximum weight you want to spend on this weapon', value=0, min=1, max=50, precision=1).props('clearable')
-    player_stats['wgt'] = weight
+    ui.number(label='Maximum weight you want to spend on this weapon', value=0, min=1, max=50, precision=1).props('clearable').bind_value(player, 'max_weight')
 
     two_handed = ui.switch(text='Do you want to use this weapon two-handed? (This will multiply your STR value for the calculation by 1.5x).')
-    if ui.switch.value:
-        player_stats['str'] = strength * 1.5
+    if two_handed:
+        player.strength = int(float(player.strength) * 1.5)
 
-    ui.button('Find your weapon!', on_click=run_weapon_optimizer)
+    player_stats = {
+                    'str': player.strength,
+                    'dex': player.dexterity,
+                    'int': player.intelligence,
+                    'fai': player.faith,
+                    'arc': player.arcane,
+                    'wgt': player.max_weight
+                   }
+    ui.button('Find your weapon!', on_click=lambda: run_weapon_optimizer(player_stats))
+
+player = Player()
 
 ui.run(reload=False)
