840b3ce91223517af33225154801d9d7a50ca095
Stephen Marsh <stephen.t.marsh@gmail.com>
2024-03-02 12:43:22 -0500

    Updates to web version style and added text

--- web/main.py
+++ web/main.py
@@ -25,7 +25,27 @@ def index():
         CALC_CORRECT_DICT = get_weapon_calc_correct_id()
 
         result = await run.cpu_bound(optimizer, player_stats)
-        ui.label(result)
+        output.clear()
+        with output:
+            ui.label(result).style('font-size: 200%; font-weight: 300')
+    
+    ui.label('Elden Ring Weapon Optimizer').classes('text-h1')
+
+    ui.label("Welcome to the Elden Ring Weapon Optimizer! This tool will figure out which weapon and infusion in Elden Ring will give you the highest AR for your character's stats by checking them against every weapon in the game. Use this as a starting point to figure out what weapon might fit your build best.")
+
+    ui.label("Note that this app is not a damage calculator - it can't figure out how much damage you'll do to a given enemy with a given attack. Also, due to how the game balances elemental infusions, this app will tend to recommend those. Check out the corresponding physical infusion(s) for the recommended weapon if you don't want the split damage.")
+    
+    with ui.row():
+        ui.label("Many thanks to u/TarnishedSpreadsheet on Reddit (Phil#5171 on Discord) for creating the data sheet this calculator uses and for his excellent walkthough of how to calculate AR in Elden Ring.")
+        ui.link('You can find his post with links to his data here.', 'https://www.reddit.com/r/Eldenring/comments/tbco46/elden_ring_weapon_calculator/')
+
+    with ui.row():
+        ui.label("Thanks also to Scott Beru, who mentored me through this project.")
+        ui.link('You can find his Github profile here.', 'https://github.com/scooberu')
+
+    ui.link("This project's repo, including a CLI version of this app and documentation, is available here.", 'https://github.com/marsherino/elden-ring-weapon-optimizer')
+
+    ui.number.default_style('min-width: 300px')
 
     ui.number(label='Strength (STR)', value=10, min=1, max=99, precision=0).props('clearable').bind_value(player, 'strength')
 
@@ -37,13 +57,15 @@ def index():
 
     ui.number(label='Arcane (ARC)', value=10, min=1, max=99, precision=0).props('clearable').bind_value(player, 'arcane')
 
-    ui.number(label='Maximum weight you want to spend on this weapon', value=0, min=1, max=50, precision=1).props('clearable').bind_value(player, 'max_weight')
+    ui.number(label='Maximum weight for this weapon', value=0, min=1, max=50, precision=1).props('clearable').bind_value(player, 'max_weight')
 
     two_handed = ui.switch(text='Do you want to use this weapon two-handed? (This will multiply your STR value for the calculation by 1.5x).')
     if two_handed:
         player.strength = int(float(player.strength) * 1.5)
 
     ui.button('Find your weapon!', on_click=lambda: run_weapon_optimizer({'str': player.strength, 'dex': player.dexterity, 'int': player.intelligence, 'fai': player.faith, 'arc': player.arcane, 'wgt': player.max_weight}))
+
+    output = ui.column()
 
 player = Player()
 
